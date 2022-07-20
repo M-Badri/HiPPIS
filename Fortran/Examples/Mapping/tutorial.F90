9623467bd301445c402f82ef80eddf237a3f6d51
ouermijudicael <ouermijudicael@gmail.com>
2022-07-20 11:59:51 -0600

    fixed errors in tutorial.F90

--- Fortran/Examples/Mapping/tutorial.F90
+++ Fortran/Examples/Mapping/tutorial.F90
@@ -13,10 +13,10 @@ program tutorial
   integer				:: interpolation_type
   integer, parameter			:: n=17
   integer, parameter			:: m=100
-  real(kind=8)				:: x(n), y(n) z(n)
-  real(kind=8)				:: v(n), v2D(n,n) v3D(n,n,n)
-  real(kind=8)				:: xout(m), yout(m) zout(m)
-  real(kind=8)				:: vout_apprx(m), v2Dout_apprx(m,m) v3Dout(m,m,m)
+  real(kind=8)				:: x(n), y(n), z(n)
+  real(kind=8)				:: v(n), v2D(n,n), v3D(n,n,n)
+  real(kind=8)				:: xout(m), yout(m), zout(m)
+  real(kind=8)				:: vout_apprx(m), vout_apprx2D(m,m), vout_apprx3D(m,m,m)
   real(kind=8)				:: eps0, eps1 
   real(kind=8)				:: dx 
   real(kind=8)				:: pi 
@@ -30,10 +30,13 @@ program tutorial
     x(i) = -pi + real(i-1, kind=8)*dx  ! input mesh points
     v(i) = sin(x(i))                   ! input data values
   enddo
+  x(n) = pi
+  v(n) = sin(x(n))
   dx = 2.0*pi/real(m-1, kind=8) 
-  do i=1, n-1
+  do i=1, m-1
     xout(i) = -pi + real(i-1, kind=8)*dx  ! output points
   enddo
+  xout(m)= pi
   
   d = 8                           ! target and maximum polynomial degree used for each interval
   interpolation_type = 2          ! 1 for DBI and 2 for PPI
@@ -58,14 +61,14 @@ program tutorial
   eps0 = 0.01                       ! optional positive parameter to bound interpolant in PPI
   eps1 = 1.0                        ! optional positive parameter to bound interpolant in PPI
 
-  call adaptiveInterpolation2D(x, y, n, n, v2D, xout, yout, m, m, vout_appx2D, d, interpolation_type, sten, eps0, eps1 ) 
+  call adaptiveInterpolation2D(x, y, n, n, v2D, xout, yout, m, m, vout_apprx2D, d, interpolation_type, sten, eps0, eps1 ) 
   
 
   !-- 3D Tutorial -- !
   z = x
-  do k=1:n
-    do j=1:n
-      do i=1:n
+  do k=1,n
+    do j=1,n
+      do i=1,n
         v3D(i,j,k) = sin(x(i))*sin(y(j))*sin(z(k))  ! input data values
       enddo
     enddo
