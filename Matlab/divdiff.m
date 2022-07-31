d740265e743081bb7bc2b655a0b7dc3e4b9b6925
ouermijudicael <ouermijudicael@gmail.com>
2022-07-31 09:06:54 -0600

    reorginizing files

---
+++ Matlab/divdiff.m
@@ -0,0 +1,45 @@
+
+function table =  divdiff(x, y, d)
+%!!! This subroutine computes the table of divided differences
+%!! 
+%!! INPUT:
+%!! x: 1D vector of x coorrdinates
+%!! y: 1D vector of y coorrdinates. y are the data values associated to the locations x.
+%!! d: maximum number of consecutive mesh points used to compute divided differences.
+%!! n: number of points in x.
+%!!
+%!! OUTPUT:
+%!!
+%!! table: array of dimension of n X (d+1)
+%!! table = u[1], u[1,2], u[1,3], ... u[1,d+1]
+%!!         u[2], u[2,3], u[2,4], ... u[2,d+2]
+%!!         u[3], u[3,4], u[3,5], ... u[2,d+3]
+%!!          .  ,    .  ,    .  , ...    .
+%!!          .  ,    .  ,    .  , ...    .
+%!!          .  ,    .  ,    .  , ...    .
+%!!         u[n],    0  ,    0  , ...    .
+
+  n = length(x);
+  table = zeros(n, d+1);
+
+
+  for i=1:n
+    table(i,1) = y(i);
+  end
+
+  for j=2:d+1
+    for i=1:n-(j-1)
+      %tmp = divdiff2(x(i:i+j-1), y(i:i+j-1), j);
+      %table(i,j) = tmp;
+
+      table(i,j) = (table(i+1, j-1)-table(i, j-1)) / (x(i+j-1)-x(i));
+      %!!if(abs(table(i, j) - tmp) > 1e-10 ) then
+      %!!  write(*,*) 'TAJO tmp =', tmp, 'table(i,j) =', table(i,j)
+      %!!  write(*,*) 'i=',i, 'j=', j
+      %!!endif
+    end
+  end
+
+end % function 
+ 
+
---
+++ Matlab/tutorial.m
@@ -0,0 +1,71 @@
+%
+clear;
+close all;
+clc;
+
+%---------------------------------------------------------------------------------------------%
+% Tutorial showing how to use the 1D, 2D, and 3D DBI and PPI interpolation
+%---------------------------------------------------------------------------------------------%
+
+  
+  %-- 1D Tutorial -- %
+  x = linspace(-pi, pi, 17);       % input mesh points
+  v = sin(x);                      % input data values
+  xout = linspace(-pi, pi, 100);   % output points
+  
+  d = 8;                           % target and maximum polynomial degree used for each interval
+  interpolation_type = 2;          % 1 for DBI and 2 for PPI
+  sten = 1;                        % optional parameter to guide stencil selection 1, 2, and 3
+  eps0 = 0.01;                     % optional positive parameter to bound interpolant in PPI
+  eps1 = 1.0;                      % optional positive parameter to bound interpolant in PPI
+
+  vout_apprx = adaptiveInterpolation1D(x, v, xout, d, interpolation_type, sten, eps0, eps1 ); 
+  
+
+  %-- 2D Tutorial -- %
+  n = 17;			     % number of input points
+  x = linspace(-pi, pi, n);          % input mesh points
+  y = x;                             
+  v2D = zeros(n,n);
+  for j=1:n
+    for i=1:n
+      v2D(i,j) = sin(x(i))*sin(y(j));  % input data values
+    end
+  end
+  xout = linspace(-pi, pi, 100);     % output points
+  yout = xout;
+  d = 8;                             % target and maximum polynomial degree used for each interval
+  interpolation_type = 2;            % 1 for DBI and 2 for PPI
+  sten = 1;                          % optional parameter to guide stencil selection 1, 2, and 3
+  eps0 = 0.01;                       % optional positive parameter to bound interpolant in PPI
+  eps1 = 1.0;                        % optional positive parameter to bound interpolant in PPI
+
+  vout_apprx2D = adaptiveInterpolation2D(x, y, v2D, xout,yout, d, interpolation_type, sten, eps0, eps1 ); 
+  
+
+  %-- 3D Tutorial -- %
+  n = 17;			     % number of input points
+  x = linspace(-pi, pi, n);          % input mesh points
+  y = x;
+  z = x;
+  v3D = zeros(n,n,n);
+  for k=1:n
+    for j=1:n
+      for i=1:n
+        v3D(i,j,k) = sin(x(i))*sin(y(j))*sin(z(k));  % input data values
+      end
+    end
+  end
+  xout = linspace(-pi, pi, 100);     % output points
+  yout = xout;
+  zout =xout;
+  d = 8;                             % target and maximum polynomial degree used for each interval
+  interpolation_type = 2;            % 1 for DBI and 2 for PPI
+  sten = 1;                          % optional parameter to guide stencil selection 1, 2, and 3
+  eps0 = 0.01;                       % optional positive parameter to bound interpolant in PPI
+  eps1 = 1.0;                        % optional positive parameter to bound interpolant in PPI
+
+  vout_apprx3D = adaptiveInterpolation3D(x, y, z, v3D, xout, yout, zout, d, interpolation_type, sten, eps0, eps1 ); 
+ 
+
+
