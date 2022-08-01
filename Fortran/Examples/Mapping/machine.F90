b49fc5f94f8c6408b50e24e0a44b22064b31f157
ouermijudicael <ouermijudicael@gmail.com>
2022-08-01 12:11:32 -0600

    added testing and fixed many files

--- Fortran/Examples/Mapping/machine.F90
+++
@@ -1,8 +0,0 @@
-module machine
-
-integer, parameter :: r8_fix    = SELECTED_REAL_KIND(12)   ! real r8
-integer, parameter :: r4_fix    = SELECTED_REAL_KIND(6,30) ! real r4
-integer, parameter :: real_kind = r8_fix
-integer, parameter :: kind_phys = real_kind
-
-end module
